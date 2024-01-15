961482fb962bf021d2d13bef2164f40fbb6ffbf4
Benjamin Lupa <147424978+BenjaminLupa@users.noreply.github.com>
2024-01-15 14:58:04 +0100

    feat: add query selectors to script-book.js

--- script-book.js
+++ script-book.js
@@ -1,3 +1,7 @@
+const btnAllBooks = document.querySelector("#btn-all-books");
+const btnFavorites = document.querySelector("#btn-favorites");
+const bookDetails = document.querySelector("#book-details");
+
 function loadBookData() {
   fetch(`http://localhost:4730/books/${isbn}`)
     .then((response) => {
