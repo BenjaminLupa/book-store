34f408294e6ff429aa008325881cf0f4776935cb
Benjamin Lupa <147424978+BenjaminLupa@users.noreply.github.com>
2024-01-16 08:50:42 +0100

    feat: add querySelector to index.js

--- script-index.js
+++ script-index.js
@@ -1,3 +1,7 @@
+const btnAllBooks = document.querySelector("#btn-all-books");
+const btnFavorites = document.querySelector("#btn-favorites");
+const BookList = document.querySelector("#book-list");
+
 function loadBookData() {
   fetch("http://localhost:4730/books")
     .then((response) => {
